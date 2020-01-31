e3d0af4e7f49779d2790ce16f5264bb85a4d9bd5
Dylan Morozowski <dylanmorozowski@Dylans-MacBook-Pro.local>
2020-01-31 13:40:23 -0500

    pushing client to github

---
+++ src/TicketmasterApi/TicketmasterIndex.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import TicketmasterResults from"./TicketmasterResults";
+
+let baseURL = "https://app.ticketmaster.com/discovery/v2/events";
+let key = "xwbbEtziqoJfipLtrIzZYHPLIAKlR3EQ";
+let secret = "GVW1oVqUo73AmA9P";
+
+const TicketmasterIndex = (props) => {
+
+    fetch("baseURL") => {
+        method: "GET",
+        body:
+        headers:
+    }
+    return (
+        <div>Return search for concerts</div>
+    )
+}
+
+export default TicketmasterIndex;
---
+++ src/TicketmasterApi/TicketmasterResults.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+TicketmasterResults = props => {
+  return <div>This will be the function that will display the results</div>;
+};
+
+export default TicketmasterResults;
